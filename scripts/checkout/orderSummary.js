0d82239049b9f3c0f1df3f48fc9181d5d499dd2b
Nitanshu12 <nitanshugoyal786@gmail.com>
2025-03-29 13:45:04 +0530

    commit

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,7 +5,6 @@ import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
-
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
 
