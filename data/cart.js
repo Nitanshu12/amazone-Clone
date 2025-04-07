be8f3929160fcd05e7bc7c59f6cdc9338ef70148
Nitanshu12 <nitanshugoyal786@gmail.com>
2025-04-07 21:12:56 +0530

    commit

--- data/cart.js
+++ data/cart.js
@@ -1,20 +1,21 @@
 export let cart;
 
-loadFromStorage()
-export function loadFromStorage(){
+loadFromStorage();
+
+export function loadFromStorage() {
   cart = JSON.parse(localStorage.getItem('cart'));
 
-if (!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionsId:'1',
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionsId:'2',
-  }];
-}
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
 }
 
 function saveToStorage() {
@@ -36,7 +37,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
-      deliveryOptionsId:'1',
+      deliveryOptionId: '1'
     });
   }
 
@@ -56,7 +57,8 @@ export function removeFromCart(productId) {
 
   saveToStorage();
 }
-export function updateDeliveryOption(productId,deliveryOptionId){
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
@@ -65,8 +67,19 @@ export function updateDeliveryOption(productId,deliveryOptionId){
     }
   });
 
-  matchingItem.deliveryOptionId=deliveryOptionId;
-
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
+}
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
 }
